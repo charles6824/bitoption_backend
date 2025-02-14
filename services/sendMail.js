69184bb916eff0c456fd4ce8edbfde4a2799aae6
Charles E. Charles <charlescharlesy@gmail.com>
2025-02-14 08:02:46 +0100

    message endpoint worked on

--- services/sendMail.js
+++ services/sendMail.js
@@ -4,16 +4,29 @@ import nodemailer from 'nodemailer'
 
 async function sendMail(to, subject, html) {
   try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
+    // const transporter = nodemailer.createTransport({
+    //   service: "gmail",
+    //   auth: {
+    //     user: "charlescharlesy@gmail.com",
+    //     pass: "ktdx mfpw mwtp zfby",
+    //   },
+    // });
+
+    let transporter = nodemailer.createTransport({
+      host: '247bitoption.com',
+      port: 465,
+      secure: true, // true for 465, false for other ports
       auth: {
-        user: "charlescharlesy@gmail.com",
-        pass: "ktdx mfpw mwtp zfby",
+          user: 'info@247bitoption.com', // generated ethereal user
+          pass: '{NS_c@QI8M}_'  // generated ethereal password
       },
+      tls:{
+        rejectUnauthorized: true
+      }
     });
 
     let mailOptions = {
-      from: '"247Bitoptions" <info@247bitoptions.com>',
+      from: '"247Bitoption" <info@247bitoption.com>',
       to: to,
       subject: subject,
       html: html,
